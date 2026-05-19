0663f5cfb9de8abd6c6e981d8ed5a18851ff3743
Hasan Md Zakaria <zakariak4@gmail.com>
2026-05-19 17:02:31 +0600

    create mongodb uri and all cars routes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8008;
+
 
 // middleware
 app.use(cors());
@@ -13,12 +15,63 @@ app.use(express.json());
 
 
 // routes
-app.get('/', (req, res) => {
-    res.send("Hello Car Rental App");
-})
+
+const uri = process.env.MONGODB_URI
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        //await client.db("admin").command({ ping: 1 });
+
+        const db = client.db('car-rental');
+
+        const carsCollection = db.collection('cars');
+
+        app.get('/', (req, res) => {
+            res.send('Car Rental Running')
+        })
+
+        app.get('/cars', async (req, res) => {
+            const cursor = carsCollection.find();
+            const result = await cursor.toArray();
+            //console.log(result);
+            res.send(result);
+        });
+
+        app.get('/cars/:carId', async (req, res) => {
+            const { carId } = req.params;
+            //console.log(carId);
+            const query = { _id: new ObjectId(carId) }
+            const result = await carsCollection.findOne(query)
+            res.send(result);
+        })
+
+
+
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
 
 
 // server
+
 app.listen(port, () => {
     console.log(`Example express js running on port ${port}`);
 });
+
+
