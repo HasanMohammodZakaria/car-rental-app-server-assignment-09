0914355fd2c695688b2df6cfc349804affb14634
Hasan Md Zakaria <zakariak4@gmail.com>
2026-05-26 16:10:00 +0600

    create delete booking API

--- index.js
+++ index.js
@@ -145,6 +145,12 @@ async function run() {
             res.json(result)
         })
 
+        app.delete('/booking/:id', async (req, res) => {
+            const { id } = req.params;
+            result = await bookingCollection.deleteOne({ _id: new ObjectId(id) })
+            res.json(result);
+        })
+
         app.delete('/cars/:id', async (req, res) => {
             const { id } = req.params
             const result = await carsCollection.deleteOne({ _id: new ObjectId(id) })
